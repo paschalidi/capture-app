67c7fbf070f4d492b56704d85e57273b4507004f
Viktor Varland <viktor@dhis2.org>
2018-02-15 16:19:26 +0100

    Remove optimistic-ui, add redux-offline, update d2/d2-ui to 29

--- src/store.js
+++ src/store.js
@@ -1,18 +1,20 @@
 // @flow
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { enableBatching } from 'redux-batched-actions';
 import { createLogger } from 'redux-logger';
 
-import reduxOptimisticUI from 'capture-core/middleware/reduxOptimisticUI/reduxOptimisticUI.middleware';
+import { offline } from '@redux-offline/redux-offline';
+import { offlineConfig } from '@redux-offline/redux-offline/lib/defaults';
+
 import { buildReducersFromDescriptions } from 'capture-core/trackerRedux/trackerReducer';
 import environments from 'capture-core/constants/environments';
 
 import reducerDescriptions from './reducers/descriptions/trackerCapture.reducerDescriptions';
 import epics from './epics/trackerCapture.epics';
 
-const middleWares = [createEpicMiddleware(epics), reduxOptimisticUI];
+const middleWares = [createEpicMiddleware(epics)];
 
 if (process.env.NODE_ENV !== environments.prod) {
     middleWares.push(
@@ -24,4 +26,9 @@ const reducersFromDescriptions = buildReducersFromDescriptions(reducerDescriptio
 
 const rootReducer = combineReducers({ ...reducersFromDescriptions });
 
-export default createStore(enableBatching(rootReducer), composeWithDevTools(applyMiddleware(...middleWares)));
+export default createStore(enableBatching(rootReducer), composeWithDevTools(
+    compose(
+        applyMiddleware(...middleWares)),
+        offline(offlineConfig)
+    )
+);
