f145f5efeb02d059aed9c1a8252297aa3794324b
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2021-01-28 14:11:53 +0100

    feat: DHIS2-10218 part 1 - IconButton, Widget, Stage Titles

--- src/components/App/AppPages.component.js
+++ src/components/App/AppPages.component.js
@@ -5,12 +5,14 @@ import { ViewEventPage } from 'capture-core/components/Pages/ViewEvent';
 import { MainPage } from 'capture-core/components/Pages/MainPage';
 import { SearchPage } from 'capture-core/components/Pages/Search';
 import { NewPage } from 'capture-core/components/Pages/New';
+import { EnrollmentDummy } from 'capture-core/components/Pages/EnrollmentDummy';
 
 export const AppPages = () => (
     <Switch>
         <Route path="/viewEvent" component={ViewEventPage} />
         <Route path="/search" component={SearchPage} />
         <Route path="/new" component={NewPage} />
+        <Route path="/enrollmentDummy" component={EnrollmentDummy} />
         <Route path="/:keys" component={MainPage} />
         <Route path="/" component={MainPage} />
     </Switch>
